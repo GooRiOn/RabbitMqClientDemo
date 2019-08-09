04381d0782b32fd17277ebcfc3155419c7e0d075
GooRiOn <foreverframenet@gmail.com>
2019-08-09 19:52:32 +0200

    Added project

---
+++ Consumer/Program.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Text;
+using RabbitMQ.Client;
+using RabbitMQ.Client.Events;
+
+namespace Consumer
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var factory = new ConnectionFactory() { HostName = "localhost", VirtualHost = "/"};
+            using(var connection = factory.CreateConnection())
+            using (var channel = connection.CreateModel())
+            {
+                channel.ExchangeDelete("hello");
+                channel.ExchangeDeclare(exchange: "hello",
+                    durable: true,
+                    autoDelete: false,
+                    arguments: null,
+                    type: "direct");
+                
+
+                channel.QueueDelete("hello.queue");
+                channel.QueueDeclare(queue: "hello.queue",
+                    durable: true,
+                    exclusive: false,
+                    autoDelete: false,
+                    arguments: null);
+                
+                channel.QueueBind("hello.queue", "hello", "#.hello.#");
+
+                var consumer = new EventingBasicConsumer(channel);
+                consumer.Received += (model, ea) =>
+                {
+                    var body = ea.Body;
+                    var message = Encoding.UTF8.GetString(body);
+                    Console.WriteLine(" [x] Received {0}", message);
+                };
+                channel.BasicConsume(queue: "hello.queue",
+                    autoAck: true,
+                    consumer: consumer);
+
+                Console.WriteLine(" Press [enter] to exit.");
+                Console.ReadLine();
+            }
+        }
+    }
+}
---
+++ Producer/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using RabbitMQ.Client;
+using RabbitMQ.Client.Framing;
+
+namespace Producer
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var factory = new ConnectionFactory()
+            {
+                HostName = "localhost"
+            };
+            
+            using(var connection = factory.CreateConnection())
+            using(var channel = connection.CreateModel())
+            {
+               
+
+                string message = "Hello World!";
+                var body = Encoding.UTF8.GetBytes(message);
+
+                channel.BasicPublish(exchange: "hello",
+                    routingKey: "#.hello.#",
+                    basicProperties: new BasicProperties
+                    {
+                        Headers = new Dictionary<string, object>
+                        {
+                            {"sdsd", "sdasdasd"}
+                        }
+                    },
+                    body: body);
+                Console.WriteLine(" [x] Sent {0}", message);
+            }
+
+            Console.WriteLine(" Press [enter] to exit.");
+            Console.ReadLine();
+        }
+    }
+}
